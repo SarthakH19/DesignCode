2276f213bf4cd5b5dc206e57ef6a519651e934ea
Sarthak <harshesarthak19@gmail.com>
2023-12-05 16:19:50 +0530

    Use Visual editor in Xcode to make a simple box

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack(alignment: .center) {
+            Image(systemName: "sparkles")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Modern architecture, an isometric tiny house, cute illustration, minimalist, vector art, night view").font(.subheadline)
+            HStack(spacing: 12.0) {
+                VStack(alignment: .leading) {
+                    Text("Size")
+                        .foregroundStyle(Color.secondary)
+                    Text("1024x1024")
+                }
+                .font(.subheadline).fontWeight(.semibold)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Type")
+                        .foregroundStyle(.secondary)
+                    Text("Upscale")
+                }
+                .font(.subheadline).fontWeight(.semibold)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Date")
+                        .foregroundStyle(.secondary)
+                    Text("Today 5:19")
+                }
+                .font(.subheadline).fontWeight(.semibold)
+            }
+            .frame(height: 44.0)
         }
-        .padding()
+        .padding(20.0)
+        .background(.green.gradient)
+        .cornerRadius(20.0)
     }
 }
 
