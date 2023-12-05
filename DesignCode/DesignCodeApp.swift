4b35258754c5329f6c7499c151c84c0c36ad2762
Sarthak <harshesarthak19@gmail.com>
2023-12-05 15:30:51 +0530

    Intialize the project

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Sarthak Harshe on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
